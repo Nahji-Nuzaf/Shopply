fd0c97e51f893178a8aa8c133da92aef6e39b6dc
Nahji Nuzaf <nahji1101@gmail.com>
2024-10-15 20:52:01 +0530

    Update forgotPasswordProcess.php

--- forgotPasswordProcess.php
+++ forgotPasswordProcess.php
@@ -2,11 +2,11 @@
 
 require "connectionshop.php";
 
-require "SMTP.php";
-require "PHPMailer.php";
-require "Exception.php";
+// require "SMTP.php";
+// require "PHPMailer.php";
+// require "Exception.php";
 
-use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\PHPMailer;
 
 if(isset($_GET["e"])){
 
@@ -20,24 +20,24 @@ if(isset($_GET["e"])){
 
         Shopply::iud("UPDATE `user` SET `verification_code`='".$code."' WHERE `email`='".$email."'");
 
-        $mail = new PHPMailer;
-        $mail->IsSMTP();
-        $mail->Host = 'smtp.gmail.com';
-        $mail->SMTPAuth = true;
-        $mail->Username = 'nahji2103842@gmail.com';
-        $mail->Password = 'lrjq xabw pfrx fxnx';
-        $mail->SMTPSecure = 'ssl';
-        $mail->Port = 465;
-        $mail->setFrom('nahji2103842@gmail.com', 'Reset Password');
-        $mail->addReplyTo('nahji2103842@gmail.com', 'Reset Password');
-        $mail->addAddress($email);
-        $mail->isHTML(true);
-        $mail->Subject = 'Shopply Forgot Password Verification Code';
-        $bodyContent = '<h1 style="color:green">Your Verification Code is '.$code.'</h1>';
-        $bodyContent .= '******************';
-        $mail->Body    = $bodyContent;
+        // $mail = new PHPMailer;
+        // $mail->IsSMTP();
+        // $mail->Host = 'smtp.gmail.com';
+        // $mail->SMTPAuth = true;
+        // $mail->Username = '*****************';
+        // $mail->Password = '*****************';
+        // $mail->SMTPSecure = 'ssl';
+        // $mail->Port = 465;
+        // $mail->setFrom('********************', 'Reset Password');
+        // $mail->addReplyTo('*******************', 'Reset Password');
+        // $mail->addAddress($email);
+        // $mail->isHTML(true);
+        // $mail->Subject = 'Shopply Forgot Password Verification Code';
+        // $bodyContent = '<h1 style="color:green">Your Verification Code is '.$code.'</h1>';
+        // $bodyContent .= '******************';
+        // $mail->Body    = $bodyContent;
 
-        if(!$mail->send()){
+        // if(!$mail->send()){
             echo("Verification Code Sending Failed.");
         }else{
             echo("success");
