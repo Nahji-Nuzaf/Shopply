1e59c158e5676c7546137f7c0ea92c12a8c61a05
Nahji Nuzaf <nahji1101@gmail.com>
2024-10-16 11:14:04 +0530

    Footer design of the project

---
+++ footer.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="bootstrap.css"/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="style.css"/>
+
+</head>
+
+<body>
+
+    <footer class="bg-black text-white pb-5 pt-4">
+
+            <div class="col-12">
+                <div class="row">
+
+                    <div class="col-10 offset-1 text-md-start">
+
+                        <div  class="row text-sm-center text-md-start">
+
+                            <div class="col-12 col-md-3 col-lg-3 mx-auto mt-3">
+                                <h5 class="align-self-start mb-1 logo" style="height: 45px;"></h5>
+                                <P class="text-center">
+                                    Shop Smart, Live Well !! by SHOPPLY&trade;
+                                </P>
+                                <P>
+                                    Welcome to SHOPPLY&trade;, Where Convenience Meets Choice, and Every Product Tells a Story.
+                                </P>
+                            </div>
+
+                            <div class="col-12 col-md-3 col-lg-2 mx-auto mt-3">
+                                <h5 class="align-self-start text-uppercase mb-4 text-warning">About Us</h5>
+                                <P >Company Info</P>
+                                <P>Privacy & Policy</P>
+                                <P>FAQs</P>
+                                <P>Support Us</P>
+                            </div>
+
+                            <div class="col-12 col-md-3 col-lg-2 mx-auto mt-3">
+                                <h5 class="align-self-start text-uppercase mb-4 text-warning">Services</h5>
+                                <P >Become an Affiliate</P>
+                                <P>Become a Seller</P>
+                                <P>Career Opportunities</P>
+                                <P>Pre Sale FAQs</P>
+                            </div>
+
+                            <div class="col-12 col-md-3 col-lg-3 mx-auto mt-3">
+                                <h5 class="align-self-start text-uppercase mb-4 text-warning">Help & Contact</h5>
+                                <p><i class="bi bi-house-fill"></i> Kensighton, Old Trafford, England</p>
+                                <p><i class="bi bi-envelope-at-fill"></i> sopply@gmail.com</p>
+                                <p><i class="bi bi-telephone-fill"></i> Contact Us</p>
+                            </div>
+
+                            <hr class="mb-4"/>
+
+                            <div class="col-12 col-md-3 col-lg-2 mx-auto mt-3">
+                                <div class="row text-sm-center text-md-start">
+
+                                    <ul class="list-unstyled list-inline">
+
+                                        <li class="list-inline-item smcircle">
+                                            <a href="#" class="form-floating text-white">
+                                                <i class="bi bi-facebook" style="font-size: 22px;"></i>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a href="#" class="form-floating text-white">
+                                                <i class="bi bi-twitter" style="font-size: 22px;"></i>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a href="https://www.instagram.com/nahji_.official/" class="form-floating text-white">
+                                                <i class="bi bi-instagram" style="font-size: 22px;"></i>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a href="#" class="form-floating text-white">
+                                                <i class="bi bi-linkedin" style="font-size: 22px;"></i>
+                                            </a>
+                                        </li>
+                                        <li class="list-inline-item">
+                                            <a href="#" class="form-floating text-white">
+                                                <i class="bi bi-youtube" style="font-size: 22px;"></i>
+                                            </a>
+                                        </li>
+
+                                    </ul>
+                                </div>
+                            </div>
+
+                            <div class="col-12 text-center">
+                                <span>
+                                    Copyright &copy; 2023 Shopply &trade; All Right Reserved.
+                                </span>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                
+
+            </div>
+        
+
+    </footer>
+
+</body>
+
+</html>
